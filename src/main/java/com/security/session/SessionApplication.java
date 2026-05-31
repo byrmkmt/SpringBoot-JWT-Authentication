f2ab7bc8a369ad17a66b7f2883b4eb95474e7fe6
Bayram Komut <byrmkmt@gmail.com>
2026-05-31 20:14:52 +0300

    0000 feature

--- src/main/java/com/security/session/SessionApplication.java
+++ src/main/java/com/security/session/SessionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SessionApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SessionApplication.class, args);
 	}
 
