1e53215531941bbd1ba30891e0b3eca8a092e6c1
Bayram Komut <byrmkmt@gmail.com>
2026-05-31 18:35:18 +0300

    unnecessary modifier is removed.

--- src/main/java/com/security/session/repository/UserRepository.java
+++ src/main/java/com/security/session/repository/UserRepository.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public boolean existsByUsername(String username);
+    boolean existsByUsername(String username);
 
-    public Optional<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 
 }
