418004e4e109efc537e4121c8556a574f0b163b3
Bayram Komut <byrmkmt@gmail.com>
2026-05-31 20:10:25 +0300

    Revert "revert merge"
    This reverts commit b9dff2a4a71af161083a32f1f4efa1bf429895a6.

--- src/main/java/com/security/session/repository/UserRepository.java
+++ src/main/java/com/security/session/repository/UserRepository.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByUsername(String username);
+    public boolean existsByUsername(String username);
 
-    Optional<User> findByUsername(String username);
+    public Optional<User> findByUsername(String username);
 
 }
